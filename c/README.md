df0d442f8edf322652c3b27e27456de265c362e3
Jack Alder <jack@jalder.com>
2025-11-18 22:53:49 -0500

    hello world

---
+++ c/README.md
@@ -0,0 +1,10 @@
+## Build
+
+```
+cmake -S. -Bcmake-build
+cmake --build cmake-build --target mdb_isready
+```
+
+### Usage
+
+./mdb_isready mongodb://localhost:27017
---
+++ c/mdb_isready.c
@@ -0,0 +1,33 @@
+#include <bson/bson.h>
+#include <mongoc/mongoc.h>
+#include <stdio.h>
+int
+main (int argc, char *argv[])
+{
+    bson_t reply;
+    bson_error_t error;
+    mongoc_init ();    
+    mongoc_client_t *client = mongoc_client_new (argv[1]);
+    if (!client) {
+        fprintf(stderr, "Failed to create client.\n");
+        return 1;
+    }
+    mongoc_client_set_appname(client, "mdb_isready");
+    bson_t *command = BCON_NEW("hello", BCON_INT32(1));
+
+    if (mongoc_client_command_simple(client, "admin", command, NULL, &reply, &error)) {
+        char *str = bson_as_legacy_extended_json(&reply, NULL);
+        printf("%s\n", str);
+        bson_free(str);
+    } else {
+        fprintf(stderr, "%s\n", error.message);
+	return 1;
+    }
+
+    bson_destroy(command);
+    bson_destroy(&reply);
+    mongoc_client_destroy(client);
+    mongoc_cleanup();
+
+    return EXIT_SUCCESS;
+}
